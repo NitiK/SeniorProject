8a366d93c7440a0acdfd9c9cbdc3ce8eea12ea27
Niti_SKE11 <kong@sinoze.com>
2017-01-12 20:40:28 +0700

    create Project

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ResetPosition() {
+		this.transform.position= new Vector3(0.0f,0.2f,-1.52f);
+		this.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f,0.0f);
+
+	}
+}
