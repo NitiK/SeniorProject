9691b2b89f9681eb71af2286bbe7966546e99a8b
niti <niti.pe@ku.th>
2017-02-22 18:34:59 -0800

    Gun

---
+++ Assets/Scripts/CameraDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraDirection : MonoBehaviour {
+	
+	Gyroscope temp;
+
+	float x,y,z;
+	// Use this for initialization
+	void Start () {
+		temp = Input.gyro;
+		temp.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//		transform.localRotation = temp.attitude;
+		Quaternion temp2 = new Quaternion(-temp.attitude.x,-temp.attitude.y,temp.attitude.z,temp.attitude.w);
+		x = temp2.eulerAngles.x;
+		y = temp2.eulerAngles.y;
+		z = temp2.eulerAngles.z;
+		transform.localRotation = Quaternion.Euler (x,y,z);
+
+		//		transform.rotation = Quaternion.Euler((temp.attitude.eulerAngles.y)*-1+90f,temp.attitude.eulerAngles.x,(temp.attitude.eulerAngles.z-90f));
+		//		string printt = "x = "+x+", y = "+y+", z = "+z;
+		//		textX.GetComponent<Text>().text = "X = "+transform.position.x;
+		//		textY.GetComponent<Text>().text = "Y = "+transform.position.y;
+		//		textZ.GetComponent<Text>().text = "Z = "+transform.position.z;
+	}
+}
