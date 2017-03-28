09ccb00328755e5f27b4af0154ee814ac0243571
niti <niti.pe@ku.th>
2017-03-29 03:49:25 +0700

    CameraAdjust

---
+++ Assets/Scripts/MapEditor/CameraAdjust.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraAdjust : MonoBehaviour {
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = transform.eulerAngles.y.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Plus(){
+		transform.Rotate (Vector3.back*10);
+		text.text = transform.eulerAngles.y.ToString();
+	}
+	public void Minus(){
+		transform.Rotate (Vector3.forward*10);
+		text.text = transform.eulerAngles.y.ToString();
+	}
+}
