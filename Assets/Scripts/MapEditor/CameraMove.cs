7f4b5ea2f4e06de07c315f7825f26a49c1db8a08
niti <niti.pe@ku.th>
2017-03-29 23:40:55 +0700

    debug CameraMove

---
+++ Assets/Scripts/MapEditor/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MoveRight(){
+		
+	}
+
+	public void MoveLeft(){
+		
+	}
+
+	public void MoveForward(){
+		transform.position += transform.forward * Time.deltaTime * 0.1f;
+	}
+
+	public void MoveBackward(){
+		transform.position -= transform.forward * Time.deltaTime * 0.1f;
+	}
+} 
