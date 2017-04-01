7fd87bbef99ec8b8c4634302fb254290375f0278
niti <niti.pe@ku.th>
2017-04-01 18:29:37 +0700

    Test light point in map

--- Assets/Scripts/MapEditor/CameraMove.cs
+++ Assets/Scripts/MapEditor/CameraMove.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 
 public class CameraMove : MonoBehaviour {
 
+	public bool clickMove;
+	public Vector3 target;
 	// Use this for initialization
 	void Start () {
-		
+		this.clickMove = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (this.clickMove) {
+			float speed = 0.1f;
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, this.target, step);
+		}
+
 	}
 
 	public void MoveRight(){
@@ -24,7 +31,19 @@ public class CameraMove : MonoBehaviour {
 	}
 
 	public void MoveForward(){
-		transform.position += transform.forward * Time.deltaTime * 0.1f;
+		this.target = transform.position + (transform.forward);
+		print (transform.position);
+		if (this.clickMove) {
+			this.clickMove = false;
+		}
+		else {
+			this.clickMove = true;
+		}
+		//print (transform.position + (transform.forward * 500000f));
+		//transform.position += transform.forward * Time.deltaTime * 0.1f;
+		/*float speed = 0.01f;
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, speed);*/
 	}
 
 	public void MoveBackward(){
