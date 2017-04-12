16d5a9f562c262026a5bcfd9dea10d7bb0489d6d
niti <niti.pe@ku.th>
2017-04-12 23:32:02 +0700

    HP player

---
+++ Assets/Scripts/Player/BarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+
+	public float fillAmount;
+
+	public Image content;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleBar ();
+	}
+
+	private void HandleBar(){
+		this.content.rectTransform.localScale = new Vector3 (this.fillAmount, 1, 1);
+	}
+}
