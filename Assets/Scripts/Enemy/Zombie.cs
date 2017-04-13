338fcd7d35259c817a76b486210b6a11e2bae546
TESTCASE\TestCase <perawithjarunithi@gmail.com>
2017-04-13 21:17:57 +0700

    Fix zombie

---
+++ Assets/Scripts/Enemy/Zombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : Enemy {
+
+	// Use this for initialization
+	void Awake () {
+		player =  GameObject.FindGameObjectWithTag ("Player").transform;
+		isDead = false;
+		startingHealth = 100f;
+		currentHealth = startingHealth;
+		movespeed = 0.05f;
+		scoreValue = 10;
+		wanderRadius = 1f;
+		detectRange = 20f;
+		attackInterval = 1.1f;
+		anime = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+}
