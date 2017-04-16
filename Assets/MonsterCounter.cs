516fa4d7a5768b6e6977a776caa9c082cc39a06f
niti <niti.pe@ku.th>
2017-04-16 23:43:32 +0700

    Update gun scene with lv2

--- Assets/MonsterCounter.cs
+++ Assets/MonsterCounter.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MonsterCounter : MonoBehaviour {
 
     public int maxMonster;
     public int killedMonster;
     public Text monsterCounterText;
+	private int clear = 0;
 
     public int KilledMonster
     {
@@ -44,10 +46,18 @@ public class MonsterCounter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         monsterCounterText.text = string.Format("Left {0} / {1}   ", killedMonster, maxMonster);
+		if (killedMonster == maxMonster) {
+			Invoke ("loadAnotherLevel", 5f);
+			this.clear = 1;
+		}
     }
 
 	public void AddKillMonster(int kill){
 		this.killedMonster += kill;
+	}
+
+	void loadAnotherLevel(){
+		Application.LoadLevel("Gun lv2");
 	}
 
 
---
+++ Assets/Scripts/Weapons/WeaponSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour {
+
+	public List<GameObject> weapon = new List<GameObject>();
+	public int TotalSpawn;
+	int count=0;
+	float timer=0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 1 && count < TotalSpawn) {
+			int randomIndex = Random.Range( 0, weapon.Count );
+			int ran = Random.Range (0, transform.childCount - 1);
+			GameObject.Instantiate (weapon[randomIndex], transform.GetChild (ran).position, transform.GetChild (ran).rotation);
+			timer -= 1;
+			count++;
+		}
+	}
+
+}
