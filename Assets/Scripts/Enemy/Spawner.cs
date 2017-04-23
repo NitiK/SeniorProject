bb9acfbcaaa44fc0707da50527bf8d20625c4d7e
niti <niti.pe@ku.th>
2017-04-24 03:41:49 +0700

    before merge

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -7,25 +7,33 @@ public class Spawner : MonoBehaviour {
 	public GameObject zombieBoss;
 	public int TotalSpawn;
 	public float delay;
+	private GameObject ground;
 	int count=0;
 	float timer=0;
 	// Use this for initialization
 	void Start () {
-		
+		ground = GameObject.FindGameObjectWithTag ("Ground");
+		Invoke ("DecreaseDelay", 30);
+	}
+
+	void DecreaseDelay() {
+		delay = delay / 2;
+		Invoke ("DecreaseDelay", 30);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
+		print (timer);
 		if (timer > delay && count < TotalSpawn-1) {
-			int ran = Random.Range (0, transform.childCount - 1);
-			GameObject.Instantiate (zombie, transform.GetChild (ran).position+new Vector3(0f,-2.2f,0f), transform.GetChild (ran).rotation);
+			int ran = Random.Range (0, ground.transform.childCount - 1);
+			GameObject.Instantiate (zombie, ground.transform.GetChild (ran).position+new Vector3(0f,-2.2f,0f), ground.transform.GetChild (ran).rotation);
 			timer -= delay;
 			count++;
 		}
 		else if (timer > delay && count < TotalSpawn) {
-			int ran = Random.Range (0 , transform.childCount - 1);
-			GameObject.Instantiate (zombieBoss, transform.GetChild (ran).position, transform.GetChild (ran).rotation);
+			int ran = Random.Range (0 , ground.transform.childCount - 1);
+			GameObject.Instantiate (zombieBoss, ground.transform.GetChild (ran).position, ground.transform.GetChild (ran).rotation);
 			timer -= delay;
 			count++;
 		}
